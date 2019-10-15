3f99fef866a0c7b36aa6f95c7eacec958bc79d95
Christian Blom <christian.blom@makingwaves.com>
2019-10-15 09:33:58 +0200

    Added code sample for push notifications from Expo docs.

--- App.tsx
+++ App.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
-}
+import { StyleSheet, Text, View, Button } from 'react-native';
+import registerForPushNotificationsAsync from './utils/RegisterPushNotifications';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
+
+const App: React.FC = () => {
+  const onPress = (): void => {
+    registerForPushNotificationsAsync();
+  };
+
+  return (
+    <View style={styles.container}>
+      <Button title="Sign up for push notifications" onPress={onPress} />
+    </View>
+  );
+};
+
+export default App;
