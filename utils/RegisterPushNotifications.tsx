bba2f0e4dd042c0e736bb2fa915ce67a45f77562
Christian Blom <christian.blom@makingwaves.com>
2019-10-16 09:24:53 +0200

    Added error handling for the Push Registration.

--- utils/RegisterPushNotifications.tsx
+++ utils/RegisterPushNotifications.tsx
@@ -1,10 +1,14 @@
-import { Notifications } from "expo";
-import * as Permissions from "expo-permissions";
-import { PUSH_ENDPOINT } from "../consts/consts";
+import { Notifications } from 'expo';
+import * as Permissions from 'expo-permissions';
+import { PUSH_ENDPOINT } from '../consts/consts';
+
+interface PushResponse {
+  error?: string;
+}
 
 const registerForPushNotificationsAsync = async (
   email: string
-): Promise<Response | void> => {
+): Promise<PushResponse> => {
   const { status: existingStatus } = await Permissions.getAsync(
     Permissions.NOTIFICATIONS
   );
@@ -12,7 +16,7 @@ const registerForPushNotificationsAsync = async (
 
   // only ask if permissions have not already been determined, because
   // iOS won't necessarily prompt the user a second time.
-  if (existingStatus !== "granted") {
+  if (existingStatus !== 'granted') {
     // Android remote notification permissions are granted during the app
     // install, so this will only ask on iOS
     const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
@@ -20,8 +24,8 @@ const registerForPushNotificationsAsync = async (
   }
 
   // Stop here if the user did not grant permissions
-  if (finalStatus !== "granted") {
-    return;
+  if (finalStatus !== 'granted') {
+    return { error: 'Permission for push notices not granted' };
   }
 
   // Get the token that uniquely identifies this device
@@ -29,18 +33,20 @@ const registerForPushNotificationsAsync = async (
 
   // POST the token to your backend server from where you can retrieve it to send push notifications.
   try {
-    console.log("POST-ing to ", PUSH_ENDPOINT);
-    return fetch(PUSH_ENDPOINT, {
-      method: "POST",
+    console.log('POST-ing to ', PUSH_ENDPOINT, 'With ', email);
+    const res = await fetch(PUSH_ENDPOINT, {
+      method: 'POST',
       headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json"
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify({
         pushToken: token,
         email
       })
     });
+    const body = await res.json();
+    return body;
   } catch (err) {
     console.error(err);
   }
